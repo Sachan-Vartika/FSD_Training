38e2fba4b204af06a5ce67e83030afd8d685946f
Sachan-Vartika <vartiika14@gmail.com>
2026-01-28 16:26:29 +0530

    javascript

---
+++ javas.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+console.log("3+4");
+
+console.error("v"); // gives error
+
+console.warn("a");
+
+let x=10;
+const v=112;
+let b=x;
+console.log(x,v,b);
+v=99;
+console.log(x,v,b);
+
